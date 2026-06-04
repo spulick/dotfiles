441000c8516454ce0544668143604c6cd37f234f
spulick <orectique@gmail.com>
2026-06-04 19:43:25 +1000

    fetch script

---
+++ copy.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+DOTFILES="$HOME/code/dotfiles"
+
+DIRS="
+$HOME/.fluxbox/init     fluxbox
+$HOME/.config/polybar   polybar
+$HOME/.config/nvim      nvim
+"
+
+
+FILES="
+$HOME/.config/starship.toml 
